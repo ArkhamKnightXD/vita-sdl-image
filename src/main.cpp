51ed49a7944770bf1c12e5a6ca2a7bd48bebbcd6
knight <karvin.jimenez@alternard.com>
2024-05-23 22:22:08 -0400

    extra changes.

--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,25 @@
 
 // Screen dimension constants
 enum {
-  SCREEN_WIDTH  = 960,
-  SCREEN_HEIGHT = 544
+    SCREEN_WIDTH  = 960,
+    SCREEN_HEIGHT = 544
 };
 
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 SDL_GameController* controller = NULL;
-
+SDL_Texture* sprite = NULL;
 SDL_Rect rectangle = {SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 32, 32};
 
 const int SPEED = 600; // Lower speed for testing
 const int FRAME_RATE = 60; // Desired frame rate (frames per second)
- 
-SDL_Texture* sprite;
 
 SDL_Texture* LoadSprite(const char* file, SDL_Renderer* renderer)
 {
     SDL_Texture* texture = IMG_LoadTexture(renderer, file);
+    if (texture == NULL) {
+        printf("Failed to load texture %s! SDL_image Error: %s\n", file, IMG_GetError());
+    }
     return texture;
 }
 
@@ -34,48 +35,47 @@ void RenderSprite(SDL_Texture* sprite, SDL_Renderer* renderer, int x, int y)
     SDL_RenderCopy(renderer, sprite, NULL, &dest);
 }
 
-
-
 // Exit the game and clean up
 void quitGame() {
-    SDL_GameControllerClose(controller);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+    if (sprite != NULL) {
+        SDL_DestroyTexture(sprite);
+    }
+    if (controller != NULL) {
+        SDL_GameControllerClose(controller);
+    }
+    if (renderer != NULL) {
+        SDL_DestroyRenderer(renderer);
+    }
+    if (window != NULL) {
+        SDL_DestroyWindow(window);
+    }
+    IMG_Quit();
     SDL_Quit();
 }
 
 // Function to handle events
 void handleEvents() {
-
     SDL_Event event;
-
     while (SDL_PollEvent(&event)) {
-
         if (event.type == SDL_QUIT) {
-            
             quitGame();
             exit(0);
         }
     }
 }
- 
+
 // Function to update rectangle movement
 void update(float deltaTime) {
-
     SDL_GameControllerUpdate();
-
     if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_UP) && rectangle.y > 0) {
         rectangle.y -= SPEED * deltaTime;
     }
-
     else if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_DOWN) && rectangle.y < SCREEN_HEIGHT - rectangle.h) {
         rectangle.y += SPEED * deltaTime;
     }
-
     else if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_LEFT) && rectangle.x > 0) {
         rectangle.x -= SPEED * deltaTime;
     }
-
     else if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_RIGHT) && rectangle.x < SCREEN_WIDTH - rectangle.w) {
         rectangle.x += SPEED * deltaTime;
     }
@@ -83,23 +83,18 @@ void update(float deltaTime) {
 
 // Function to render graphics
 void render() {
-    // Clear the renderer
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    
     SDL_RenderClear(renderer);
-
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-
+        // Draw everything on a dark blue background
+    SDL_SetRenderDrawColor(renderer, 0, 0, 128, 255);
+        
     RenderSprite(sprite, renderer, 30, 30);
-
-    SDL_RenderFillRect(renderer, &rectangle);
 
     SDL_RenderPresent(renderer);
 }
 
 void capFrameRate(Uint32 frameStartTime) {
-
     Uint32 frameTime = SDL_GetTicks() - frameStartTime;
-    
     if (frameTime < 1000 / FRAME_RATE) {
         SDL_Delay(1000 / FRAME_RATE - frameTime);
     }
@@ -108,25 +103,37 @@ void capFrameRate(Uint32 frameStartTime) {
 int main(int argc, char *argv[])
 {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER) < 0) {
+        printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
         return -1;
     }
 
-    if ((window = SDL_CreateWindow("RedRectangle", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN)) == NULL) {
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+        printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
         return -1;
     }
 
-    if ((renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED)) == NULL) {
+    window = SDL_CreateWindow("RedRectangle", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
         return -1;
     }
 
     sprite = LoadSprite("sprites/sprite.png", renderer);
+    if (sprite == NULL) {
+        printf("Failed to load sprite image!\n");
+        return -1;
+    }
 
     if (SDL_NumJoysticks() < 1) {
         printf("No game controllers connected!\n");
         return -1;
-    } 
-    else {
-
+    } else {
         controller = SDL_GameControllerOpen(0);
         if (controller == NULL) {
             printf("Unable to open game controller! SDL Error: %s\n", SDL_GetError());
@@ -140,7 +147,6 @@ int main(int argc, char *argv[])
 
     // Main loop
     while (1) {
-
         currentFrameTime = SDL_GetTicks();
         deltaTime = (currentFrameTime - previousFrameTime) / 1000.0f;
         previousFrameTime = currentFrameTime;
@@ -148,7 +154,6 @@ int main(int argc, char *argv[])
         handleEvents();
         update(deltaTime);
         render();
-
         capFrameRate(currentFrameTime);
     }
 
